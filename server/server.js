6e5834dc5c2b5c99db66ad8b768eca09dfc19942
abi <abimael.monarrez58@gmail.com>
2022-04-06 20:48:44 -0600

    add route for offline.html

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,5 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../client/dist/offline.html'))
+);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
